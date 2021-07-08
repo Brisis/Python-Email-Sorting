5055ea2cdf6b49c1359c9e4c77acffa9857fce60
Brisis <38928449+Brisis@users.noreply.github.com>
2021-07-08 12:58:53 +0200

    Add files via upload

---
+++ sorting.py
@@ -0,0 +1,27 @@
+import re
+import pandas as pd
+
+#read emails csv
+data = pd.read_csv('unsorted_emails.csv')
+
+#get first column of data only
+df = data.iloc[:,0]
+
+#flatten column to get list of emails
+#Url list initialization
+Input = df.values.flatten()
+#Python code to sort the URL in the list based on the top-level domain.
+
+#Function to sort in tld order
+def tld(Input):
+	return Input.split('@')[-1]
+
+#Using sorted and calling function
+Output = sorted(Input,key=tld)
+
+# save to a new csv file
+pd.DataFrame(Output).to_csv("sorted_emails.csv", header=None, index=None)
+
+#Printing output
+print("sorted list according to TLD is")
+print(Output)
